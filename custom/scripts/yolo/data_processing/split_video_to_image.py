15ccbf20374434914199764728a122a853494370
Vacuame <836610245@qq.com>
2026-01-05 10:00:28 +0800

    优化图片分割效率

--- custom/scripts/yolo/data_processing/split_video_to_image.py
+++ custom/scripts/yolo/data_processing/split_video_to_image.py
@@ -3,7 +3,7 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 
-def extract_frames_every_n(video_path, n=5):
+def extract_frames_every_n(video_path, step=5):
     # 获取视频文件名（不含扩展名）和所在目录
     video_dir = os.path.dirname(video_path)
     video_basename = os.path.splitext(os.path.basename(video_path))[0]
@@ -19,23 +19,23 @@ def extract_frames_every_n(video_path, n=5):
         return
     
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_count = 0
     saved_count = 0
+    current_frame = 0
 
-    while True:
+    while current_frame < total_frames:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
         ret, frame = cap.read()
         if not ret:
             break  # 视频结束
 
         # 每隔 n 帧保存一次（从第0帧开始，即第0,5,10,...帧）
-        if frame_count % n == 0:
-            img_name = f"{video_basename}_{saved_count}.jpg"
-            img_path = os.path.join(output_folder, img_name)
-            cv2.imwrite(img_path, frame)
-            saved_count += 1
-            print(f"已完成 {frame_count} / {total_frames}  {frame_count/ total_frames * 100:.2f}%")
+        img_name = f"{video_basename}_{saved_count}.jpg"
+        img_path = os.path.join(output_folder, img_name)
+        cv2.imwrite(img_path, frame)
+        saved_count += 1
+        print(f"已完成 {current_frame} / {total_frames}  {current_frame/ total_frames * 100:.2f}%")
 
-        frame_count += 1
+        current_frame += step
 
     cap.release()
     print(f"完成！共保存 {saved_count} 张图片到文件夹：{output_folder}")
@@ -51,7 +51,7 @@ def select_video_and_process():
     if video_path:
         if not video_path.lower().endswith('.mp4'):
             print("警告：选择的文件不是 .mp4 格式，但仍尝试处理...")
-        extract_frames_every_n(video_path, 15)
+        extract_frames_every_n(video_path, 60)
     else:
         print("未选择任何文件。")
 
