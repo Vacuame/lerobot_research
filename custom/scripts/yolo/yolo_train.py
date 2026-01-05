c7d1313477c1f6ae2f544c9ae10dcaeafcbe84a2
Vacuame <836610245@qq.com>
2026-01-05 14:25:34 +0800

    新的训练设置

--- custom/scripts/yolo/yolo_train.py
+++ custom/scripts/yolo/yolo_train.py
@@ -1,28 +1,33 @@
 from ultralytics import YOLO
 
 
-def yolo_train(data_yaml):
+def yolo_train():
     # Load a model
     model = YOLO("yolo11n-seg.yaml")  # build a new model from YAML
     model = YOLO("yolo11n-seg.pt")  # load a pretrained model (recommended for training)
     model = YOLO("yolo11n-seg.yaml").load("yolo11n-seg.pt")  # build from YAML and transfer weights
 
-    # Train the model
-    model.train(
-        data=data_yaml,
-        epochs=150,
-        imgsz=640,
-        freeze=10,        # ★ 冻结 backbone
-        lr0=1e-4,         # ★ 小学习率
-        patience=30,
-        workers=0
+    # 这个用于极小数据量，freeze设为10，学习率很小且固定
+    # model.train(      
+    #     data=data_yaml,
+    #     epochs=150,
+    #     imgsz=640,
+    #     freeze=10,        # ★ 冻结 backbone
+    #     lr0=1e-4,         # ★ 小学习率
+    #     patience=30,
+    #     workers=0
+    # )
+    model.train( # 新尝试的yaml设置训练
+        data="coco8-seg.yaml",
+        cfg="custom/scripts/yolo/train.yaml",
+        name="grab_block"
     )
 
 def yolo_train_resume(path):
     model = YOLO(path)  # load a trained model "runs/segment/train2/weights/last.pt"
-    results = model.train(resume=True)  # resume training from last.pt
+    model.train(resume=True)  # resume training from last.pt
 
 if __name__ == "__main__":
-    yolo_train("coco8-seg.yaml")
+    yolo_train()
 
     
